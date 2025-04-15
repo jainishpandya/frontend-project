e0a428ac75df812403134c3b382554a39faa8bd5
Ekansh Singh <ekansh.singh@hs.local>
2025-04-15 18:12:41 +0530

    color modifications in dashboard components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='w-full h-13 bg-blue-900 flex justify-center items-center'>
+    <div className='w-full h-13 bg-br-blue-medium flex justify-center items-center'>
       <div className='text-white'>Copyrights are reserved by <span className='font-bold'>BookCircle</span></div>
     </div>
   )
